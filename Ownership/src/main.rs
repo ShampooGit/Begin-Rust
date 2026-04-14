183b285a76846dbd821958d23891b13df9d6655b
Shampoo <76459124+ShampooGit@users.noreply.github.com>
2026-04-14 18:24:30 +0200

    Basic Memory stuff again
    altough this is very easy and clear i do need to get used to the -> String,
    
    next up we go to refrences less goooo

--- Ownership/src/main.rs
+++ Ownership/src/main.rs
@@ -76,10 +76,155 @@ fn main() {
 */
 
 //  String ver
+/*
 fn main() {
     let s1 = String::from ("Hello");
     let s2 = s1;
-    // this does not simple copy the data like with the int
+    // this does not simply copy the data like with the int
+    // We instead copy the "name tag" info (pointer, len, cap)
     // this is beacuse a string is an array and its a pointer to the data thats being stored on the heap
     // so in the end we just coppy the memory adress instead of the data itself
+    // if i really had to copy the data i could use .clone
 }
+// to prevent double memory Freeing Rust says yo s1 fuck you your now invaled (except if i use .clone)
+// so this is called a move, 
+*/
+/*
+fn main () {
+    let s1 = String::from("Hello");
+    let s2 = s1;
+    println!("{s1} , world!");
+    // if i try to run this
+    // compiler says hey this is not possible add .clone behind s1  ->  let s2 = s1.clone;
+    // this is cuz we moved from the string s1 to s2
+}
+*/
+
+
+//
+//  crazy discovery
+//
+//  there is something called .len
+//  wich gives the length of an array that means i cna use it on strings right
+//  also what about the other to things that are stored there like cap and pointer
+//  i must be able to show those aswell somehow...
+//
+
+
+
+//
+//  Scope & Assignment
+//
+/*
+fn main() {
+    let mut s = String::from("Hello"); // this got dropped
+    s = String::from("Ahoy");
+    println!("{s}, World");
+}
+*/
+
+//
+//  Var and data interact with clone
+//
+/*
+fn main() {
+    let s1 = String::from("Hello");
+    let s2 = s1.clone();
+    println!("s1 = {s1} & s2 = {s2}");
+}
+*/
+//  usesing clone can be very expensive
+
+
+
+//
+//  Stack-Only Data : Copy
+//
+/*
+fn main() {
+    let x = 5;
+    let y = x;
+    println!("x = {x} & y = {y}"); 
+}
+// there is a magic word called copy wich can copy scaler data types
+// for like way cheaper then clone cuz it only works on stack saved data types
+*/
+
+
+//
+//  Ownership & Functions
+//
+/*
+fn main() {
+    let s = String::from("Hello");
+    take_ownership(s);  // s has been moved to the function
+    // println!("{s}"); <- this is not possible
+    
+    let x = 5;
+    makes_copy(x); // x is copied to the function
+    println!("{x}"); // this is possible cuz we copy scaler types
+} // Here, x goes out of scope, then s. However, because s's value was moved,
+  // nothing special happens.
+
+fn take_ownership(Val_1: String){
+    println!("{Val_1}");
+} // Here, some_string goes out of scope and `drop` is called. The backing
+  // memory is freed.
+
+fn makes_copy(Val_1: u32){
+    println!("{Val_1}");
+} // Here, some_integer goes out of scope. Nothing special happens.
+*/
+
+
+//
+//  Return Values & Scope
+//
+/*
+fn main() {
+    let s1 = gives_ownership(); // returns a value
+
+    let s2 = String::from("Hello"); // sets a value
+
+    let s3 = takes_and_gives_back(s2); // moves a value & returns it
+} // Here, s3 goes out of scope and is dropped. s2 was moved, so nothing
+  // happens. s1 goes out of scope and is dropped.
+
+fn gives_ownership() -> String { // -> is used to indicate the returing type
+    let val_1 = String::from("yours");
+    val_1 
+}
+
+// 
+fn takes_and_gives_back(val_1: String) -> String {
+    val_1 // val_1 is returned and moves out to the calling function
+}
+*/
+
+
+//
+//  returning mutple values with a tuple
+//
+
+/*
+its says that retunring a value like this (Pasing the ownership back and forth), 
+is anoying. i agree, they mention using tuples as another thing we can do
+to return mutple values. i wonder when we will get into refrences tho.
+*/
+
+fn main() {
+    let s1 = String::from("Hello");
+
+    let (s1, len) = calculate_length(s1);
+
+    println!("the word '{s1}' has a length of {len}");
+}
+
+fn calculate_length(val_1: String) -> (String, usize) {// because we returning mutiple values we use ()
+    let length = val_1.len(); // .len() returns the lentgh of a string
+
+    (val_1, length)
+}
+
+// yesss now we go onto refrences finally...
+
