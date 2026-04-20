e94b3a497bac2320867ed3bf8d931a4a74ce529c
Shampoo <76459124+ShampooGit@users.noreply.github.com>
2026-04-21 01:25:37 +0200

    mutable refrences
    okay thats fire they way it workd is great and love the fact that i can not have &mut and & in the same scope unless i ahve alreadu used either one in its section

--- Ref-and-Borro/src/main.rs
+++ Ref-and-Borro/src/main.rs
@@ -74,6 +74,94 @@ fn change(s: &String) {
 //  Mutable Refrences
 //
 
+// to fix the last code snippet we can do &mut
+/*
+fn main() {
+    let mut s = String::from("Hello");
+    change(&mut s);
+}
+
+fn change(a_string :&mut String) {
+    a_string.push_str(", world");
+}
+*/
+// so s becomes a &mut
+// in the signature change(a_string: &mut String)
+// so a_string: wordt the var in the fn
+// en &mut String laat zien van hey dit is een 
+// refrence mutable en het is een String
+
+// the one big restriction
+// there can only be one &mut to that s var
+
+// so this would not work
+/*
+fn main() {
+    let mut s = String::from("Hello");
+    let r1 = &mut s;
+    let r2 = &mut s;
+
+    println!("{r1},{r2}");
+}
+*/
+
+// the reason for this 
+// it prevents data races and makes the code 
+// more reliable
+/*
+fn main() {
+    let mut s = String::from("Hello");
+    {
+        let r1 = &mut s;
+    }
+    let r2 = &mut s;
+}
+// this works cuz r1 is in a diffrent scope
+*/
+
+
+// another thing
+// there is a simaler rule like the one above
+// for combining muable and imutable refrences
+/*
+fn main() {
+    let mut s = String::from("Hello");
+    let r1 = &s;
+    let r2 = &s;
+    let r3 = &mut s;
+    println!("{r1}, {r2}, and {r3}");
+}
+// so r3 is causing a problem 
+*/
+/*
+fn main() {
+    let mut s = String::from("Hello");
+
+    let r1 = &s;
+    let r2 = &s;
+    println!("{r1} and {r2}");
+// since this r1 & r2 do not get used after this print
+// we can do a &mut to s
+
+    let r3 = &mut s;
+    r3.push_str(", World");  
+    println!("{r3}");
+// the same happens here if we dont use r3 after this print
+// we are all good ofc i can reintilise them
+
+    let r1 = &s;
+    let r2 = &s;
+    println!("{r1} and {r2}");
+// but now the value will be ofc changed
+}
+*/
+
+
+
+//
+//  Dangling Refrences
+//
+
 fn main() {
     
 }
