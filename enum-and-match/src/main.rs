6048f73ecea0c0c534fe1a81474d0219e357c3f3
Shampoo <76459124+ShampooGit@users.noreply.github.com>
2026-05-20 21:33:15 +0200

    got more stuff done on enums
    this shitt crazyyy bruf wthh but next time we do more

--- enum-and-match/src/main.rs
+++ enum-and-match/src/main.rs
@@ -1,3 +1,8 @@
+//
+//  enums 
+//
+
+/*
 enum IpAddrKind {
     V4,
     V6,
@@ -28,4 +33,101 @@ fn main() {
 fn route(ip_kind: IpAddrKind) { // here we say okay ip_kind is of type IpAddrKind
     // now thats dope as fuck love this idea 
 }
+*/
 
+/*
+fn main() {
+    enum IpAddrKind {
+        V4,
+        V6,
+    }
+
+    struct IpAddr {
+        kind: IpAddrKind,
+        adress: String,
+    }
+
+    // here we make home have the version and we assign a adress
+    let home = IpAddr {
+        kind: IpAddrKind::V4,
+        adress: String::from("127.0.0.1"),
+    };
+
+    let loopback = IpAddr {
+        kind: IpAddrKind::V6,
+        adress: String::from("::1"),
+    };
+
+    // and here we print them out
+    println!("ip adress of home {}", home.adress);
+    println!("ip adress of loopback {}", loopback.adress);
+}
+
+*/
+
+// how ever we can write it shorter and more concise if we tell the version
+// what type of data it will store so check this
+/*
+fn main() {
+    enum IpAddr {
+        V4(String),
+        V6(String),
+    }
+
+    let home = IpAddr::V4(String::from("127.0.0.1"));
+
+    let loopback = IpAddr::V6(String::from("::1"));
+    /*
+        here we attach data to the variant enum home = (V4)127.0.0.1
+        so there is not need for a struct since we already say hey this variant 
+        has this data type attached to it
+
+        and it reutrns a instance of IpAddr
+        also the varaintions become fucntions that take a String and return a
+        instance of IpAddr
+
+    */
+}
+*/
+
+//
+//  now comes something really cool 
+//
+/*
+
+fn main() {
+    enum IpAddr {
+        // we can have mutiple in one enum variantion
+        V4(u8, u8, u8, u8),
+        V6(String),
+    }
+
+    let home = IpAddr::V4(127, 0, 0, 1);
+
+    let loopback = IpAddr::V6(String::from("::1"));
+}
+*/
+
+//
+//  alot can be in a enum
+//
+
+fn main() {
+
+    struct Ipv4Addr {
+
+    }
+    struct Ipv6Addr {
+
+    }
+    enum IpAddr {
+        V4(Ipv4Addr),
+        V6(Ipv6Addr),
+    }
+    /*
+    this shows that enums can store a fuck ton of diffrent things 
+    like a struct wtf is this, thats alot of power 
+    gonna be honest dont even know how to to but damnn
+    its gonna be fire when i do.
+    */
+}
