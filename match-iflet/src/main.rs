ab235ed6ead4802787c330bdab4111c1dc2bc08a
Shampoo <76459124+ShampooGit@users.noreply.github.com>
2026-06-12 17:27:32 +0200

    finished chapter 6
    holy moly wth was that last bit ima take a break now cuz reading this made me want to stop my god. but defo usefull in some cases.

--- match-iflet/src/main.rs
+++ match-iflet/src/main.rs
@@ -24,22 +24,23 @@ fn main() {
 //  in the coin exmaple 
 //
 
-[derive(Debug)]
-enum Ustate {
-    Alabama
-    Alaska
-    Arizona
+#[derive(Debug)]
+enum UsState {
+    Alabama,
+    Alaska,
+    Arizona,
+    Arkansas,
 }
 
 enum Coin {
     Penny,
     Nickel,
     Dime,
-    Quarter(Ustate),
+    Quarter(UsState),
 }
 
 fn main() {
-    let coin = Coin:Penny;
+    let coin = Coin::Penny;
     let mut count = 0;
     // match version
     /*
@@ -59,3 +60,71 @@ fn main() {
 //
 //  The Happy path  let else
 //
+
+// v1
+// this version does alot in once and is hard to read 
+// thats why v2 is a thing 
+// this really is complex T-T
+/*
+fn describe_state_quarter(coin: Coin) -> Option<String> {
+    if let Coin::Quarter(state) = coin {
+        if state.existed_in(1900) {
+            Some(format!("{state:?} is pretty old, for America!"))
+        } else {
+            Some(format!("{state:?} is relativley new."))
+        }
+    } else {
+        None
+    }
+}
+*/
+// v2
+// here we assign the if let to another value early,
+// so the state coin lives inside the var state now.
+// but this is apperantly still to much to write so there is a v3
+/*
+fn describe_state_quarter(coin: Coin) -> Option<String> {
+    let state = if let Coin::Quarter(state) = coin {
+        state
+    } else {
+        return None;
+    };
+    // i do get the v3 from this one tho cuz there are 2 ifs that are connected
+    if state.existed_in(1900) {
+        Some(format!("{state:?} is pretty old, for America!"))
+    } else {
+        Some(format!("{state:?} is relativley new."))
+    }
+}
+*/
+// v3
+// now id where let...else comes in we start with let
+// ... represents code 
+// else is well the else part
+// the let...else does not have an if breabjc only a else branch
+
+fn describe_state_quarter(coin: Coin) -> Option<String> {
+    let Coin::Quarter(state) = coin else {
+        return None;
+    };
+
+    if state.existed_in(1900) {
+        Some(format!("{state:?} is pretty old, for America"))
+    } else {
+        Some(format!("{state:?} is relativley new."))
+    }
+}
+// this if let and let...else are tools for the match 
+// pretty hgandy but i do feel liek theese are pretty complex for what they do
+// there is just so much to remember 
+
+impl UsState {
+    fn existed_in(&self, year: u16) -> bool{
+        match self {
+            UsState::Alabama => year >= 1819,
+            UsState::Alaska => year >= 1959,
+            UsState::Arizona => year >= 1912,
+            _ => false,
+        }
+    }
+}
