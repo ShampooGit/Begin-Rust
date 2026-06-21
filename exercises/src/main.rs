65768cb8a6f744a320efff676e48fe4155addbde
Shampoo <76459124+ShampooGit@users.noreply.github.com>
2026-06-21 22:56:11 +0200

    5 tasks over the week
    so i did 5 tasks they were okay i did struggle with the fifth one tho but i hope that tmwr ima get the parts i orderd online to do some work on those if i dont get them this week ill do some other tasks

---
+++ exercises/src/main.rs
@@ -0,0 +1,105 @@
+
+// main
+fn main() {
+    // task 5
+    let mut number = Some(42);
+    if let Some(val) = number {
+        println!("{val}");
+        number = None
+    } else {
+        println!("oops");
+    }
+    // task 4
+    reply(HttpReply::Success);
+    reply(HttpReply::NotFound);
+    reply(HttpReply::InternalError);
+
+    // task 3
+    let mut user1 = User {
+        username: String::from("None"),
+        login_count: 0,
+    };
+    println!("{}, {}", user1.login_count, user1.username);
+    user1.login_count = 1;
+    user1.username = String::from("Bob");
+    println!("{}, {}", user1.login_count, user1.username);
+    
+    // task 2
+    let ref_string = String::from("Kaas");
+    print_var(&ref_string);
+    println!("{}", ref_string);
+    
+    
+    // task 1
+    check_score(67);
+}
+
+
+/*
+Exercise 1: Score Checker
+The Goal: Write basic control flow and logic from scratch.
+
+Task: Write a function check_score(score: u32) that prints 
+"Pass" if the score is 50 or higher, and "Fail" if it is lower.
+*/
+fn check_score(score: u32) {
+    if score >= 50 {
+        println!("you passed");
+    } else {
+        println!("you failed");
+    }
+}
+
+/*
+Exercise 2: Ownership Pass-Along
+The Goal: Feel how ownership moves and gets blocked by the compiler.
+
+Task: Create a String variable in main. Pass it to a function that just prints it. 
+Try to print that same variable again in main after the function call. 
+Watch the compiler yell at you, then fix it by changing the function to use a reference instead.
+*/
+fn print_var(string_var: &String) {
+    println!("{}", string_var);
+}
+
+
+/*
+Exercise 3: The User Struct
+The Goal: Write a data structure and read/modify its fields.
+
+Task: Create a struct named User with fields for username and login_count. 
+In main, create an instance of this struct, change the login count, and print the username.
+*/
+
+struct User {
+    username: String,
+    login_count: u32
+}
+
+/*
+Exercise 4: Web Traffic Enum
+Task: Create an enum HttpReply with variants: Success, NotFound, and InternalError. 
+Use a match statement inside a function or main to print a custom text message for each one.
+*/
+
+enum HttpReply {
+    Success,
+    NotFound,
+    InternalError,
+}
+
+fn reply(status: HttpReply) {
+    match status {
+        HttpReply::Success => println!("Omg it was a success"),
+        HttpReply::NotFound => println!("http reply not found"),
+        HttpReply::InternalError => println!("Opps there was an error"), // we could use the other thing we did witht he coins for specific error
+    }
+}
+
+
+/*
+Exercise 5: The Safety Net (Option)
+Task: Create a variable holding Some(42). 
+Use an if let statement to extract the number and print it. 
+Then change that variable to None to make sure your code handles it without crashing.
+*/
