f46b1ab0077bb3a00a8d123a1e6d5717df5bf6ec
Shampoo <76459124+ShampooGit@users.noreply.github.com>
2026-07-12 23:01:45 +0200

    did some stuff not alot
    it was a little struggle to find out how crates work i had to look at examples to see how i should implement it but idk that did not feel right like i was missing something

--- exercises/src/main.rs
+++ exercises/src/main.rs
@@ -1,6 +1,11 @@
 
 // main
 fn main() {
+    // task 7
+    let _gradient_text = String::from("░░░▒▒▒▓▓▓▒▒▒░░░");
+    pretty_text(&_gradient_text);
+    random_color_text(String::from("Color my text"),&_gradient_text);
+
     // task 6
     let temp = 25;
     let humidity = 20; 
@@ -34,6 +39,19 @@ fn main() {
     
     // task 1
     check_score(67);
+
+
+    // task 0 
+    //print_flag();
+}
+
+fn print_flag() {
+
+    println!("{}", "█████████████".truecolor(91, 206, 250));
+    println!("{}", "█████████████".truecolor(245, 169, 184));
+    println!("{}", "█████████████".truecolor(255, 255, 255));
+    println!("{}", "█████████████".truecolor(245, 169, 184));
+    println!("{}", "█████████████".truecolor(91, 206, 250));
 }
 
 
@@ -105,10 +123,10 @@ Task: Create a variable holding Some(42).
 Use an if let statement to extract the number and print it. 
 Then change that variable to None to make sure your code handles it without crashing.
 */
-fn response(mut n: Option<i32>) {
+fn response(/*mut*/ n: Option<i32>) {
     if let Some(val) = n {
         println!("{val}");
-        n = None
+        // n = None
     } else {
         println!("oops");
     }
@@ -139,4 +157,42 @@ fn plant_status(t: i32, h: i32) {
         Facestate::Thirsty => println!("plss give me some water"),
         Facestate::Sleepy => println!("oops ima sleep"),
     }
+}
+
+/*
+Exercise 7: simple crate usage
+Task 1: implement the rand crate        ( https://docs.rs/rand/latest/rand/ )
+Task 2: implement the colored crate     ( https://docs.rs/colored/latest/colored/ )
+Task 3: implement the chrono crate      ( https://docs.rs/chrono/latest/chrono/ )
+Task 4: combine all crates into one
+*/
+
+use colored::Colorize;
+
+fn random_color_text(string: String, g_string: &String) {
+
+    let rand_red: u8  = rand::random();
+    let rand_green: u8  = rand::random();
+    let rand_blue: u8  = rand::random();
+    println!("red = {},   green = {},   blue = {}", rand_red, rand_green, rand_blue);
+    println!("{}", {g_string}.truecolor(rand_red, rand_green, rand_blue));
+    println!("{}",{string}.truecolor(rand_red,rand_green,rand_blue));
+}
+
+fn pretty_text(string: &String) {
+    let mut red: u8 = 255;
+    let green: u8 = 95;
+    let mut blue: u8 = 0;
+
+    for _i in 1..6 {
+        blue = blue + 51;
+        println!("{}", {string}.truecolor(red, green, blue));
+    }
+    for _i in 1..5 {
+        red = red - 51;
+        println!("{}", {string}.truecolor(red, green, blue));
+    }
+    println!("");
+    println!("{}", {string}.truecolor(red, green, blue));
+    println!("{}", "Einde".truecolor(red, green, blue));
 }
